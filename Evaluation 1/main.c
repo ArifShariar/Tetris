885eecd17fbd64ccdbd5731feb59a6b7823bc5b1
Sadia <1705102@ugrad.cse.buet.ac.bd>
2021-06-25 12:43:33 +0600

    communication done

--- Evaluation 1/main.c
+++ Evaluation 1/main.c
@@ -29,12 +29,48 @@ const char column[64]={
 char i,scan = 0x01;
 int main(void)
 {
-	DDRA = 0XFF;
-    DDRB = 0XFF;
-	DDRC = 0XFF;
-	DDRD = 0XFF;
+
+	DDRD = 0X00;
+	DDRD |= 1 << PIND1;//pin1 of portD as OUTPUT
+	DDRD &= ~(1 << PIND0);//pin0 of portD as INPUT
+	PORTD |= 1 << PIND0;
+	int UBBRValue = 25;//AS described before setting baud rate
+	//Put the upper part of the baud number here (bits 8 to 11)
+	UBRRH = (unsigned char) (UBBRValue >> 8);
+	//Put the remaining part of the baud number here
+	UBRRL = (unsigned char) UBBRValue;
+	//Enable the receiver and transmitter
+	UCSRB = (1 << RXEN) | (1 << TXEN);
+	//Set 2 stop bits and data bit length is 8-bit
+
+	UCSRC = (1 << USBS) | (3 << UCSZ0);
     while (1) 
     {
+		
+		if (PIND & 0x10)//once button is pressed
+
+		{
+
+			while (! (UCSRA & (1 << UDRE)) );
+
+			{
+
+				UDR = 0b11110000;//once transmitter is ready sent eight bit data
+
+			}
+
+			// Get that data outa here!
+
+			_delay_ms(220);
+		
+		}
+		
+		
+		
+		DDRA = 0XFF;
+		DDRB = 0XFF;
+		DDRC = 0XFF;
+		DDRD = 0XFF;
 		scan = 0x01;
 		for (i=0;i<=64;i=i+8)
 		{
